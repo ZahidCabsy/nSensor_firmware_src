1e92ba4ba06cb4bcf2f4b00d56379095072deca3
zahid h <z.hussain@cabsy.com>
2023-01-01 20:48:18 +0400

    fixed tabs

--- src/main.cpp
+++ src/main.cpp
@@ -21,36 +21,36 @@ static auto UHD     = esp32cam::Resolution::find(1600, 1200);
 
 bool handshake(int handshakeTimeout)
 {
-  char receivedByte;
-  auto startTime = millis();
+	char receivedByte;
+  	auto startTime = millis();
 
-  while (true)
-  {
-    if (Sender.available())
-    {
-      receivedByte = Sender.read();
-      if (receivedByte == HANDSHAKE_BYTE)
-      {
-        Serial.println("G! ");
-        break;
-      }
-    }
+  	while (true)
+  	{
+    	if (Sender.available())
+    	{
+      	receivedByte = Sender.read();
+      	if (receivedByte == HANDSHAKE_BYTE)
+      	{
+        		Serial.println("G! ");
+        		break;
+      	}
+    	}
 
-    if (millis() - startTime > static_cast<unsigned long>(handshakeTimeout))
-    {
-      break;
-    }
-  }
+    	if (millis() - startTime > static_cast<unsigned long>(handshakeTimeout))
+    	{
+      	break;
+    	}
+  	}
 
-  if (receivedByte == HANDSHAKE_BYTE)
-  {
-    return true;
-  }
-  else
-  {
-    Serial.printf("Handshake Serial timeOut!!!");
-    return false;
-  }
+  	if (receivedByte == HANDSHAKE_BYTE)
+  	{
+    	return true;
+  	}
+  	else
+  	{
+    	Serial.printf("Handshake Serial timeOut!!!");
+    	return false;
+  	}
 }
 
 int getResponse(int bufferSize)
@@ -76,11 +76,13 @@ int getResponse(int bufferSize)
         		break;
       	}
     	}
-    	if (millis() - startTime > static_cast<unsigned long>(kResponseTimeout))
+    		
+		if (millis() - startTime > static_cast<unsigned long>(kResponseTimeout))
     	{
       	break;
     	}
    }
+	
 	Serial.printf("Handshake serial timeout!");
   	return 1;
 }
@@ -115,10 +117,12 @@ bool handleReadAmbient()
         		Sender.write(ambient);
         		break;
       	}
+
       	if (RX == 0x44) // D
       	{
          	Sender.write(ambLen);
       	}
+
       	if (millis() - startTime > static_cast<unsigned long>(kAmbientReadTimeout))
       	{
         		break;
@@ -129,7 +133,7 @@ bool handleReadAmbient()
     	{
       	break;
     	}
-  	}
+  	 }
    
 	Serial.printf("Ambient read timeout!");
   	return false;
@@ -150,10 +154,12 @@ bool handleGetSensorFirmwareVersion(char *sensor_version, char *ver_length)
         		Sender.write(sensor_version);
         		break;
       	}
+
       	if (RX == 0x44) // D
       	{
          	Sender.write(ver_length);
       	}
+
       	if (millis() - startTime > static_cast<unsigned long>(kVersionSyncTimeout))
       	{
         		break;
@@ -215,8 +221,8 @@ void getFrame()
 	auto frame = esp32cam::capture();
   	if (frame == nullptr)
   	{
-   	Serial.println("CAPTURE FAIL");
-    	return;
+   		Serial.println("CAPTURE FAIL");
+    		return;
   	}
 
 	frameSize = static_cast<int>(frame->size());
@@ -235,15 +241,15 @@ void getFrame()
   	Sender.write(bufferSizeLengthBuffer);
   	if (handshake(2000) != true)
   	{
-   	Serial.println("Buffer Size Length Handshake Fail");
-    	return;
+   		Serial.println("Buffer Size Length Handshake Fail");
+    		return;
   	}
-   /*After handshake send the size of captured frame*/
+  	/*After handshake send the size of captured frame*/
   	Sender.write(bufferSize);
   	if (handshake(2000) != true)
   	{
-   	Serial.println("Buffer Size Handshake Fail");
-    	return;
+   		Serial.println("Buffer Size Handshake Fail");
+    		return;
   	}
 
   	constexpr int chunkSize = 4096;
@@ -256,34 +262,34 @@ void getFrame()
    	printf("Start %d : \n", (counter - 1));
 	 	
 		/*Inform the ncontroller of outgoing frame chunk size and wait for response*/
-    	rc = getResponse(chunkSize);
+    		rc = getResponse(chunkSize);
 		
 		/*If ncontroller sends acknowlegement start sending frame*/
 		if (rc == 0)
     	{
-      	Sender.write(&frame->data()[counter - 1], chunkSize);
+   		Sender.write(&frame->data()[counter - 1], chunkSize);
 
-      	if (remaining == 0)
-      	{
-        		printf("TS");
-        		break;
-      	}
-      	else
-      	{
-      		remaining = remaining - chunkSize;
-        		printf("remaining : %d\n", remaining);
-        		if (remaining > 0)
-        		{
-         		counter += chunkSize;
-        		}
-        		else
-        		{
-         		int finalChunkSize = chunkSize + remaining;
-          		counter += chunkSize;
-          		printf("Final : %d\n", finalChunkSize);
-          		remaining = 0;
-        		}
-      	}
+   		if (remaining == 0)
+   		{
+     			printf("TS");
+     			break;
+   		}
+   		else
+   		{
+   			remaining = remaining - chunkSize;
+     			printf("remaining : %d\n", remaining);
+     			if (remaining > 0)
+     			{
+      			counter += chunkSize;
+     			}
+     			else
+     			{
+      			int finalChunkSize = chunkSize + remaining;
+       			counter += chunkSize;
+       			printf("Final : %d\n", finalChunkSize);
+       			remaining = 0;
+     			}
+   		}
     	}
     	else
     	{
@@ -295,116 +301,118 @@ void getFrame()
 
 void getNsensorMacSerialno(char *sensor_mac, char *mac_length, char *ver_length)
 {
-  Serial.println ();
-  Serial.println ("Reading Mac");
-  byte count = 0;
+  	Serial.println ();
+  	Serial.println ("Reading Mac");
+  	byte count = 0;
  
-  /*I2C address for MAC Eeprom**/
-  uint8_t a = 94;
+  	/*I2C address for MAC Eeprom**/
+  	uint8_t a = 94;
   
-  /**I2C register address- Starting position for MAC byte*/
-  uint8_t v = 154;
+  	/**I2C register address- Starting position for MAC byte*/
+  	uint8_t v = 154;
   
-  /**Number of bytes (MAC)*/
-  uint8_t byt = 6;
+  	/**Number of bytes (MAC)*/
+  	uint8_t byt = 6;
 
-  uint8_t macbuff[6];
+  	uint8_t macbuff[6];
 
-  int length;
+  	int length;
   
-  Wire.begin();
+  	Wire.begin();
 
-  for (byte i = 8; i < 120; i++)
-  {
-    Wire.beginTransmission (i);        // Begin I2C transmission Address (i)
-    if (Wire.endTransmission () == 0)  // Receive 0 = success (ACK response) 
-    {
-      Serial.print ("Found address: ");
-      Serial.print (i, DEC);
-      Serial.print (" (0x");
-      Serial.print (i, HEX);     // PCF8574 7 bit address
-      Serial.println (")");
-      count++;
-    }
-  }
-  Serial.print ("Found ");      
-  Serial.print (count, DEC);        // numbers of devices
-  Serial.println (" device(s).");
-  Wire.beginTransmission (a);
-  Wire.write(v);
-  Wire.endTransmission ();
-  Wire.requestFrom(a, byt);
+  	for (byte i = 8; i < 120; i++)
+  	{
+    	Wire.beginTransmission (i);        // Begin I2C transmission Address (i)
+    	if (Wire.endTransmission () == 0)  // Receive 0 = success (ACK response) 
+    	{
+      	Serial.print ("Found address: ");
+      	Serial.print (i, DEC);
+      	Serial.print (" (0x");
+      	Serial.print (i, HEX);     // PCF8574 7 bit address
+      	Serial.println (")");
+      	count++;
+    	}
+  	}
+  	Serial.print ("Found ");      
+  	Serial.print (count, DEC);        // numbers of devices
+  	Serial.println (" device(s).");
+  	Wire.beginTransmission (a);
+  	Wire.write(v);
+  	Wire.endTransmission ();
+  	Wire.requestFrom(a, byt);
   
-  if (Wire.available() != 6) {
-    Serial.print("Mac Readerror");
-  }
+  	if (Wire.available() != 6) 
+	{
+    	Serial.print("Mac Readerror");
+  	}
 
-  for(int x=0; x<6; x++) {
-    macbuff[x] = Wire.read();
-    Serial.print (macbuff[x], HEX);
-  }
+  	for(int x=0; x<6; x++) 
+	{
+    	macbuff[x] = Wire.read();
+    	Serial.print (macbuff[x], HEX);
+  	}
 
-  sprintf(sensor_mac,"%x:%x:%x:%x:%x:%x", macbuff[0], macbuff[1], macbuff[2], macbuff[3], macbuff[4], macbuff[5]);
-  length = strlen(sensor_mac);
-  sprintf(mac_length, "%i", length);
-  Serial.println(sensor_mac);
-  Serial.print(" = ");
-  Serial.println(mac_length);
+  	sprintf(sensor_mac,"%x:%x:%x:%x:%x:%x", macbuff[0], macbuff[1], macbuff[2], macbuff[3], macbuff[4], macbuff[5]);
+  	length = strlen(sensor_mac);
+  	sprintf(mac_length, "%i", length);
+  	Serial.println(sensor_mac);
+  	Serial.print(" = ");
+  	Serial.println(mac_length);
 
-  sprintf(sensor_version,"%i.%i.%i", V_APP, V_MAJOR, V_MINOR);
-  length = strlen(sensor_version);
-  sprintf(ver_length, "%i", length);
-  Serial.println(sensor_version);
+  	sprintf(sensor_version,"%i.%i.%i", V_APP, V_MAJOR, V_MINOR);
+  	length = strlen(sensor_version);
+  	sprintf(ver_length, "%i", length);
+  	Serial.println(sensor_version);
 
 }
 
 void handleGetRawCameraImage()
 {
-  Serial.println("OK!");
-  Sender.write(0x50);
-  getFrame();
+	Serial.println("OK!");
+  	Sender.write(0x50);
+  	getFrame();
 }
 
 /*Change here while compiling, based on the sensor device requirements*/
 void handleGetSensorType()
 {
-  Sender.write(FULL_OPTION_TYPE);
+  	Sender.write(FULL_OPTION_TYPE);
 }
 
 void handleChangeResolution()
 {
-  Serial.println("Change Resolution");
-  ESP.restart();
+  	Serial.println("Change Resolution");
+  	ESP.restart();
 }
 
 /*This will run only once during start up*/
 void setup()
 {
-  /**Setting the baud rate for serial console*/
-  Serial.begin(115200); // Terminal
+  	/**Setting the baud rate for serial console*/
+  	Serial.begin(115200); // Terminal
 
-  /*Setting the baud rate for serial data transmission **/
-  Sender.begin(BIT_RATE, SERIAL_8N1, 13, 12); // RX TX communication box - esp
+  	/*Setting the baud rate for serial data transmission **/
+  	Sender.begin(BIT_RATE, SERIAL_8N1, 13, 12); // RX TX communication box - esp
 
 
- /**Setting up camera
-  * sda= GPIO_04 
-  * scl= GPIO_14
-  */
-  Wire.begin (GPIO_04, GPIO_14);  
-  {
-    using namespace esp32cam;
-    Config cfg;
-    cfg.setPins(pins::AiThinker);
-    cfg.setResolution(semisqr);
-    cfg.setBufferCount(1);
-    cfg.setJpeg(80);
-    bool ok = Camera.begin(cfg);
-    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
-  }
+ 	/**Setting up camera
+  	* sda= GPIO_04 
+  	* scl= GPIO_14
+  	*/
+ 	Wire.begin (GPIO_04, GPIO_14);  
+  	{
+    	using namespace esp32cam;
+    	Config cfg;
+ 		cfg.setPins(pins::AiThinker);
+ 		cfg.setResolution(semisqr);
+ 		cfg.setBufferCount(1);
+ 		cfg.setJpeg(80);
+ 		bool ok = Camera.begin(cfg);
+ 		Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+  	}
 
-  /**Here it gets MAC address and serial number of the nsensor*/
-  getNsensorMacSerialno((sensor_mac, mac_length, ver_length);
+	/**Here it gets MAC address and serial number of the nsensor*/
+  	getNsensorMacSerialno((sensor_mac, mac_length, ver_length);
 }
 
 /**This is the main app code, It loops here for the input from the 
